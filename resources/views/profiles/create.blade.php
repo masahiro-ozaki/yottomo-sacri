28df69a0b89d6059ae617c348f6fd37b66eb60f3
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-07-04 05:13:19 +0000

    hometeam

--- resources/views/profiles/create.blade.php
+++ resources/views/profiles/create.blade.php
@@ -20,21 +20,97 @@
         
         <div class="form-group">
             {!! Form::label('birthplace', 'ホームチーム:') !!}
-            <!--<table class="table table-striped">-->
-            <!--    <tr>-->
-            <!--        <td>1-A</td>-->
-            <!--        <td>{{ Form::radio('birthplace', '1-A') }}</td>-->
-            <!--    </tr>-->
-            <!--    <tr>-->
-            <!--        <td>1-B</td>-->
-            <!--        <td>{{ Form::radio('birthplace', '1-B') }}</td>-->
-            <!--    </tr>-->
-            <!--    <tr>-->
-            <!--        <td>1-C</td>-->
-            <!--        <td>{{ Form::radio('birthplace', '1-C') }}</td>-->
-            <!--    </tr>-->
-            <!--</table>-->
-            {{ Form::select('birthplace', array(''=>'選択してください','1-A'=>'1-A','1-B'=>'1-B','1-C'=>'1-C','2-A'=>'2-A','2-B'=>'2-B','2-C'=>'2-C','3-A'=>'3-A','3-B'=>'3-B','3-C'=>'3-C','4-A'=>'4-A','4-B'=>'4-B','4-C'=>'4-C','5-A'=>'5-A','5-B'=>'5-B','5-C'=>'5-C','6-A'=>'6-A','6-B'=>'6-B','6-C'=>'6-C','7-A'=>'7-A','7-B'=>'7-B','7-C'=>'7-C','8-A'=>'8-A','8-B'=>'8-B','8-C'=>'8-C','9-A'=>'9-A','9-B'=>'9-B','9-C'=>'9-C','10-A'=>'10-A','10-B'=>'10-B','10-C'=>'10-C','11-A'=>'11-A','11-B'=>'11-B','11-C'=>'11-C','12-A'=>'12-A','12-B'=>'12-B','12-C'=>'12-C','13-A'=>'13-A','13-B'=>'13-B','13-C'=>'13-C','14-A'=>'14-A','14-B'=>'14-B','14-C'=>'14-C','15-A'=>'15-A','15-B'=>'15-B','15-C'=>'15-C') )}}
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>1</th>
+                        <th>2</th>
+                        <th>3</th>
+                        <th>4</th>
+                        <th>5</th>
+                        <th>6</th>
+                        <th>7</th>
+                        <th>8</th>
+                        <th>9</th>
+                        <th>10</th>
+                        <th>11</th>
+                        <th>12</th>
+                        <th>13</th>
+                        <th>14</th>
+                        <th>15</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>A</td>
+                        <td>{{ Form::radio('birthplace', '1-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '2-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '3-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '4-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '5-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '6-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '7-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '8-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '9-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '10-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '11-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '12-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '13-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '14-A') }}</td>
+                        <td>{{ Form::radio('birthplace', '15-A') }}</td>
+                    </tr>
+                    <tr>
+                        <td>B</td>
+                        <td>{{ Form::radio('birthplace', '1-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '2-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '3-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '4-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '5-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '6-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '7-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '8-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '9-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '10-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '11-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '12-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '13-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '14-B') }}</td>
+                        <td>{{ Form::radio('birthplace', '15-B') }}</td>
+                    </tr>
+                    <tr>
+                        <td>C</td>
+                        <td>{{ Form::radio('birthplace', '1-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '2-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '3-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '4-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '5-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '6-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '7-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '8-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '9-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '10-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '11-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '12-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '13-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '14-C') }}</td>
+                        <td>{{ Form::radio('birthplace', '15-C') }}</td>
+                    </tr>
+                </tbody>
+                <!--<tr>-->
+                <!--    <td>1-A</td>-->
+                <!--    <td>{{ Form::radio('birthplace', '1-A') }}</td>-->
+                <!--</tr>-->
+                <!--<tr>-->
+                <!--    <td>1-B</td>-->
+                <!--    <td>{{ Form::radio('birthplace', '1-B') }}</td>-->
+                <!--</tr>-->
+                <!--<tr>-->
+                <!--    <td>1-C</td>-->
+                <!--    <td>{{ Form::radio('birthplace', '1-C') }}</td>-->
+                <!--</tr>-->
+            </table>
+            <!--{{ Form::select('birthplace', array(''=>'選択してください','1-A'=>'1-A','1-B'=>'1-B','1-C'=>'1-C','2-A'=>'2-A','2-B'=>'2-B','2-C'=>'2-C','3-A'=>'3-A','3-B'=>'3-B','3-C'=>'3-C','4-A'=>'4-A','4-B'=>'4-B','4-C'=>'4-C','5-A'=>'5-A','5-B'=>'5-B','5-C'=>'5-C','6-A'=>'6-A','6-B'=>'6-B','6-C'=>'6-C','7-A'=>'7-A','7-B'=>'7-B','7-C'=>'7-C','8-A'=>'8-A','8-B'=>'8-B','8-C'=>'8-C','9-A'=>'9-A','9-B'=>'9-B','9-C'=>'9-C','10-A'=>'10-A','10-B'=>'10-B','10-C'=>'10-C','11-A'=>'11-A','11-B'=>'11-B','11-C'=>'11-C','12-A'=>'12-A','12-B'=>'12-B','12-C'=>'12-C','13-A'=>'13-A','13-B'=>'13-B','13-C'=>'13-C','14-A'=>'14-A','14-B'=>'14-B','14-C'=>'14-C','15-A'=>'15-A','15-B'=>'15-B','15-C'=>'15-C') )}}-->
         </div>
         
         <div class="form-group">
